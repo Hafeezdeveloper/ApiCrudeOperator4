078f5133b99ada174db573c463c3288e2d55766d
Hafeezdeveloper <Hafeez.hussain2003@gmail.com>
2023-03-01 14:15:11 +0500

    childToParent

--- src/confrig/Posts.js
+++ src/confrig/Posts.js
@@ -5,56 +5,33 @@ import { Delete, Gets, Post, Put, Puts } from '../comp/DataHandle'
 
 
 const Posts = () => {
-  const [isLoading, setLoading] = useState(false)
-  const [openButton, setButtonOpen] = useState(true)
-
-  const [data, setData] = useState([])
-
-  const getDataaa = () => {
-  // Put   200 means Success
-  //   Puts('posts', '1', JSON.stringify({
-  //   id: 1,
-  //   title: 'foo',
-  //   body: 'bar',
-  //   userId: 1,
-  //   }) ).then((succ) => console.log(succ)
-  //       // setData([...succ.data])
-  //       // setLoading(false)
-  //     )
-  //     .catch((err) => console.log(err))
-
-  //       // Gets('posts').then((suc) => console.log(suc)).catch((res) => console.log(res))
-  // }
-  // Post   201 means Success
-  // Post('posts', JSON.stringify({
-  //   title: 'foo',
-  //   body: 'bar',
-  //   userId: 1,
-  //   }) ).then((succ) => console.log(succ)
-  //       // setData([...succ.data])
-  //       // setLoading(false)
-  //     )
-  //     .catch((err) => console.log(err))
-
-  //       // Gets('posts').then((suc) => console.log(suc)).catch((res) => console.log(res))
+  // const [isLoading, setLoading] = useState(false)
+  var [openButton, setButtonOpen] = useState(false)
+  var [opens, setOpens] = useState(false)
   
-  // Delete('posts','1',JSON.stringify({
-  //      title: 'foo',
-  //   body: 'bar',
-  //   userId: 1,
-  // })).then((succ) => console.log(succ)) 
-  // .catch( (err) => console.log(err))
+  
+ 
+  
+  
+  var setButtonOpen = (e)=>{
+    if(e === true){
+      setOpens(true)
+    }else{
+      setOpens(false)
+    }
+   
   }
-
-  useEffect(() => {
-    getDataaa()
-  }, [])
-
+  
   return (
     <div>
       <Box>
-      <Buttons innerChl={  <TextField  />} dataSend={(e) => setButtonOpen(false)} opens={openButton}>+</Buttons> 
-      {isLoading ? (
+    <Buttons opens={opens} setButtonOpen={setButtonOpen}>+</Buttons> 
+
+
+
+
+      {/* <Buttons  dataSend={(e) => console.log(e)} opens={openButton} >+</Buttons>  */}
+      {/* {isLoading ? (
         <img src='https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif?20170503175831' alt="loader" />
         ) : data && data.lenght < 1 ? (<h1>No data found ...</h1>)
         : (data && data.length > 0 ? (data.map((x, i) => {
@@ -64,7 +41,7 @@ const Posts = () => {
               <p>{x.body}</p>
             </div>
           )
-        })) : null)}
+        })) : null)} */}
 
         </Box>
     </div>
